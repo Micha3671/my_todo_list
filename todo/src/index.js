994011a689c4fd3955f7d00270fbdc34745ef0d6
Micha <michael.rigas@t-online.de>
2024-02-07 16:01:36 +0100

    Login-delay

--- todo/src/index.js
+++ todo/src/index.js
@@ -1,16 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import App from "./App";
 import Login from "./Login";
 
-ReactDOM.render(
-  <Router>
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="/login" element={<Login />} />{" "}
-      {/* Hier wird die Login-Seite angezeigt */}
-    </Routes>
-  </Router>,
-  document.getElementById("root")
-);
+const AppRouter = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/login" element={<LoginWithDelay />} />
+      </Routes>
+    </Router>
+  );
+};
+
+const LoginWithDelay = () => {
+  const [shouldRenderLogin, setShouldRenderLogin] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShouldRenderLogin(true);
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  return shouldRenderLogin ? <Login /> : <div>Loading...</div>;
+};
+
+ReactDOM.render(<AppRouter />, document.getElementById("root"));
